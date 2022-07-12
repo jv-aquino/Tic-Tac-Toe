88fcddf54d81140c7bdbab8ea8ce4e0b08105384
jv-aquino <jvaquinocontact@gmail.com>
2022-07-12 14:50:49 -0300

    Add functions to populate gameboard and DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,57 @@
-const Gameboard = (() => {
-  const gameboardArray = [];
-  const squares = document.querySelectorAll('.square');
+const Game = (() => {
+  const getActivePlayer = () => {
+    return playerX.getLastClick() ? playerO : playerX;
+  };
+  const toggleActivePlayer = () => {
+    playerX.toggleLastClick();
+    playerO.toggleLastClick();
+  };
 
-  squares.forEach(square => {
-    gameboardArray.push(square);
-  });
+  const markSquare = (e) => {
+    let squareId = e.target.id;
+
+    let player = getActivePlayer();
+    toggleActivePlayer();
+
+    let symbol = player.getSymbol();
+
+    let content = Gameboard.getSquareContent(squareId);
+
+    if (content == '') {
+      Gameboard.setSquareContent(squareId, symbol)
+    }
+  };
+
+  return {markSquare};
 })();
 
-const Player = (symbol) => {
+const Gameboard = (() => {
+  const gameboardArray = Array.from(document.querySelectorAll('.square'));
+  gameboardArray.forEach(square => {
+    square.addEventListener('click', Game.markSquare);
+  });
+  
+  const getSquareContent = (squareId) => {
+    return gameboardArray[squareId].textContent;
+  };
 
+  const setSquareContent = (squareId, symbol) => { 
+    gameboardArray[squareId].textContent = symbol;
+  };
+
+  return {getSquareContent, setSquareContent};
+})();
+
+const Player = (symbol, lastClick) => {
+  const getSymbol = () => symbol;
+  const getLastClick = () => lastClick;
+
+  const toggleLastClick = () => {
+    lastClick = lastClick ? false : true;
+  }
+
+  return {getSymbol, getLastClick, toggleLastClick};
 };
+
+const playerX = Player('X', false);
+const playerO = Player('O', true);
