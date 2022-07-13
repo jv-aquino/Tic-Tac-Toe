64984dab551b950929ad13106e17dbcabe4087e9
jv-aquino <jvaquinocontact@gmail.com>
2022-07-13 10:48:00 -0300

    Add check winner function and UI for columns and rows

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,32 @@
 const Game = (() => {
+  let winner = null;
+  let activeGame = true;
+
+  const printWinner = (winner) => {
+    activeGame = false;
+    const winnerDiv = document.querySelector('.winner');
+
+    winnerDiv.textContent = 'Player ' + winner + ' won!';
+
+    winnerDiv.style.marginBottom = '30px';
+  };
+
+  const checkGameOver = () => {
+    // The winner is altered only if there's a winner
+    if (!(Gameboard.checkColumns() == null)) {
+      winner = Gameboard.checkColumns();
+      printWinner(winner);
+    }
+    else if (!(Gameboard.checkRows() == null)) {
+      winner = Gameboard.checkRows();
+      printWinner(winner);
+    }
+    else if (!(Gameboard.checkDiagonals() == null)) {
+      winner = Gameboard.checkDiagonals();
+      printWinner(winner);
+    }
+  };
+
   const getActivePlayer = () => {
     return playerX.getLastClick() ? playerO : playerX;
   };
@@ -11,16 +39,17 @@ const Game = (() => {
     let squareId = e.target.id;
 
     let player = getActivePlayer();
-
     let symbol = player.getSymbol();
 
     let content = Gameboard.getSquareContent(squareId);
 
-    if (content == '') {
+    if (content == '' && activeGame) {
       Gameboard.setSquareContent(squareId, symbol);
     
       toggleActivePlayer();
     }
+
+    checkGameOver();
   };
 
   return {markSquare};
@@ -40,7 +69,54 @@ const Gameboard = (() => {
     gameboardArray[squareId].textContent = symbol;
   };
 
-  return {getSquareContent, setSquareContent};
+  const getGameboard = () => gameboardArray;
+
+  const checkColumns = () => {
+    let columns = [];
+    let winner = null;
+
+    for(let i = 0; i <= 2; i++) {
+      columns[i] = [gameboardArray[i], gameboardArray[i + 3], gameboardArray[i + 6]]
+    }
+
+    columns.forEach(column => {
+      const result = column.every(element => {
+        if (element.textContent == column[0].textContent && element.textContent != '') {
+          return true;
+        }
+      });
+      winner = result ? column[0].textContent : winner;
+    });
+    return winner;
+  }
+
+  const checkRows = () => {
+    let rows = [];
+    let winner = null;
+
+    for(let i = 0; i <= 6; i = i + 3) {
+      rows[i] = [gameboardArray[i], gameboardArray[i + 1], gameboardArray[i + 2]]
+    }
+
+    rows.forEach(row => {
+      const result = row.every(element => {
+        if (element.textContent == row[0].textContent && element.textContent != '') {
+          return true;
+        }
+      });
+      winner = result ? row[0].textContent : winner;
+    });
+    return winner;
+  };
+
+  const checkDiagonals = () => {
+    let diagonals = [];
+    let winner = null;
+
+    return null;
+  };
+
+  return {getSquareContent, setSquareContent, getGameboard, checkColumns, checkDiagonals, checkRows};
 })();
 
 const Player = (symbol, lastClick) => {
