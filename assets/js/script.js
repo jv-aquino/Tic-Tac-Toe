a46a48c743f2731db89f6d9602765528c4ff8f61
jv-aquino <jvaquinocontact@gmail.com>
2022-07-19 09:48:33 -0300

    Adds functional UI

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,62 @@
+let player1, player2;
+
+const Settings = (() => {
+  const xButton = document.querySelector('#X');
+  const oButton = document.querySelector('#O');
+
+  const checkStartButtonUI = () => {
+    if (p1Name.value != '' && p2Name.value != '') {
+      start.classList.add('abled')
+    }
+    else {
+      start.classList.remove('abled')
+    }
+  };
+
+  const startGame = () => {
+    if (p1Name.value != '' && p2Name.value != '') {
+      Game.setActiveGame(true);
+
+      p1Symbol = (xButton.className == 'abled') ? 'X' : 'O';
+      p2Symbol = (xButton.className == 'disabled') ? 'X' : 'O';
+
+      let p1FirstMove = (xButton.className == 'abled') ? true : false;
+
+      player1 = Player(p1Symbol, p1FirstMove, p1Name.value);
+      player2 = Player(p2Symbol, !p1FirstMove, p2Name.value);
+    }
+  }
+  const toggleSymbol = (e) => {
+    xButton.classList.toggle('abled');
+    xButton.classList.toggle('disabled');
+
+    oButton.classList.toggle('abled');
+    oButton.classList.toggle('disabled');
+  };
+
+  const p1Name = document.querySelector('#p1');
+  const p2Name = document.querySelector('#p2');
+  p1Name.addEventListener('keyup', checkStartButtonUI);
+  p2Name.addEventListener('keyup', checkStartButtonUI);
+
+  const symbolsButtons = Array.from(document.querySelectorAll('.symbols button'));
+  const start = document.querySelector('#start');
+
+  start.addEventListener('click',startGame);
+
+  symbolsButtons.forEach(button => {
+    button.addEventListener('click', toggleSymbol);
+  });
+
+  return {};
+})();
+
 const Game = (() => {
   let winner = null;
-  let activeGame = true;
+  let activeGame = false;
 
   const printWinner = (winner) => {
-    activeGame = false;
-    
+    setActiveGame(false);
     const winnerDiv = document.querySelector('.winner');
 
     if (winner == 'Tie') {
@@ -21,15 +73,24 @@ const Game = (() => {
     // The winner is altered only if there's a winner
     if (!(Gameboard.checkColumns() == null)) {
       winner = Gameboard.checkColumns();
-      printWinner(winner);
+      
+      let winnerName = (player1.getSymbol() == winner) ? player1.getName() : player2.getName();
+
+      printWinner(winnerName);
     }
     else if (!(Gameboard.checkRows() == null)) {
       winner = Gameboard.checkRows();
-      printWinner(winner);
+      
+      let winnerName = (player1.getSymbol() == winner) ? player1.getName() : player2.getName();
+
+      printWinner(winnerName);
     }
     else if (!(Gameboard.checkDiagonals() == null)) {
       winner = Gameboard.checkDiagonals();
-      printWinner(winner);
+      
+      let winnerName = (player1.getSymbol() == winner) ? player1.getName() : player2.getName();
+
+      printWinner(winnerName);
     }
     else if (!(Gameboard.checkTie() == null)) {
       printWinner('Tie');
@@ -37,22 +98,22 @@ const Game = (() => {
   };
 
   const getActivePlayer = () => {
-    return playerX.getLastClick() ? playerO : playerX;
+    return player1.getLastClick() ? player1 : player2;
   };
   const toggleActivePlayer = () => {
-    playerX.toggleLastClick();
-    playerO.toggleLastClick();
+    player1.toggleLastClick();
+    player2.toggleLastClick();
   };
 
   const markSquare = (e) => {
     let squareId = e.target.id;
 
-    let player = getActivePlayer();
-    let symbol = player.getSymbol();
-
     let content = Gameboard.getSquareContent(squareId);
 
     if (content == '' && activeGame) {
+      let player = getActivePlayer();
+      let symbol = player.getSymbol();
+
       Gameboard.setSquareContent(squareId, symbol);
     
       toggleActivePlayer();
@@ -61,7 +122,11 @@ const Game = (() => {
     checkGameOver();
   };
 
-  return {markSquare};
+  const setActiveGame = (value) => {
+    activeGame = value;
+  };
+
+  return {markSquare, setActiveGame};
 })();
 
 const Gameboard = (() => {
@@ -151,16 +216,14 @@ const Gameboard = (() => {
   return {getSquareContent, setSquareContent, getGameboard, checkColumns, checkDiagonals, checkRows, checkTie};
 })();
 
-const Player = (symbol, lastClick) => {
+const Player = (symbol, lastClick, name) => {
   const getSymbol = () => symbol;
   const getLastClick = () => lastClick;
+  const getName = () => name;
 
   const toggleLastClick = () => {
     lastClick = lastClick ? false : true;
   }
 
-  return {getSymbol, getLastClick, toggleLastClick};
+  return {getSymbol, getLastClick, toggleLastClick, getName};
 };
-
-const playerX = Player('X', false);
-const playerO = Player('O', true);
